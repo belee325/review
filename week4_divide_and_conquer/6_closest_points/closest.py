0ceb31d6ca3dd068c4c6803386439d342d484c0d
belee325 <belee325@gmail.com>
2018-02-28 16:58:04 -0500

    Add files via upload

--- week4_divide_and_conquer/6_closest_points/closest.py
+++ week4_divide_and_conquer/6_closest_points/closest.py
@@ -2,7 +2,7 @@
 import sys
 import math
 
-def minimum_distance(x, y):
+def minimum_distance(points):
     #write your code here
     return 10 ** 18
 
@@ -10,6 +10,6 @@ if __name__ == '__main__':
     input = sys.stdin.read()
     data = list(map(int, input.split()))
     n = data[0]
-    x = data[1::2]
-    y = data[2::2]
-    print("{0:.9f}".format(minimum_distance(x, y)))
+    points = [(x,y) for x,y in zip(data[1::2],data[2::2])] 
+    points.sort(key = lambda tup: tup[0])
+    print("{0:.9f}".format(minimum_distance(points)))
