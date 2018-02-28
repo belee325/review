d5ba1bd00dcbf15db306ace1200bc3aba0201b2e
belee325 <belee325@gmail.com>
2018-02-28 16:57:41 -0500

    Add files via upload

--- week4_divide_and_conquer/5_organizing_a_lottery/points_and_segments.py
+++ week4_divide_and_conquer/5_organizing_a_lottery/points_and_segments.py
@@ -1,10 +1,29 @@
 # Uses python3
 import sys
+import math
 
 def fast_count_segments(starts, ends, points):
-    cnt = [0] * len(points)
-    #write your code here
-    return cnt
+    count = [0] * len(points)
+    temp =[]
+    for x,y in zip(starts,ends):
+        temp.append((x,'l'))
+        temp.append((y,'r'))
+    for p in points:
+        temp.append((p,'p'))
+    print(temp)
+    temp.sort(key = lambda tup:(tup[0],tup[1]))
+    print(temp)
+    counter = 0
+    for pt in temp:
+        if pt[1]=='l':
+            counter +=1
+        elif pt[1]=='p':
+            count[points.index(pt[0])] = counter
+            points[points.index(pt[0])] = math.nan
+        else:
+            counter -= 1
+            counter = max(0,counter)
+    return count
 
 def naive_count_segments(starts, ends, points):
     cnt = [0] * len(points)
