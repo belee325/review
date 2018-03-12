391b8702fef8a5c453228edd1f7eff82f79ef0ff
Lee <blee13@metlife.com>
2018-03-12 15:30:28 -0400

    prob 1

--- ps_2/make_heap/build_heap.py
+++ ps_2/make_heap/build_heap.py
@@ -23,16 +23,34 @@ class HeapBuilder:
     # but in the worst case gives a quadratic number of swaps.
     #
     # TODO: replace by a more efficient implementation
-    for i in range(len(self._data)):
-      for j in range(i + 1, len(self._data)):
-        if self._data[i] > self._data[j]:
-          self._swaps.append((i, j))
-          self._data[i], self._data[j] = self._data[j], self._data[i]
-
+    #for i in range(len(self._data)):
+    # for j in range(i + 1, len(self._data)):
+    #    if self._data[i] > self._data[j]:
+    #      self._swaps.append((i, j))
+    #      self._data[i], self._data[j] = self._data[j], self._data[i]
+    for i in reversed(range(0,len(self._data)//2)):
+        while True:
+            #left child = 2i + 1, right child = 2i +2
+            min_idx = i
+            left = 2*i+1
+            right = left + 1
+            if left < len(self._data) and self._data[min_idx] >= self._data[left]:
+                min_idx = left 
+            if right < len(self._data) and self._data[min_idx] >= self._data[right]: 
+                min_idx = right
+            if min_idx != i:
+                self._data[i],self._data[min_idx] = self._data[min_idx],self._data[i]
+                self._swaps.append((i,min_idx))
+                i = min_idx
+            else:
+                break
+    return
+    
   def Solve(self):
     self.ReadData()
     self.GenerateSwaps()
     self.WriteResponse()
+
 
 if __name__ == '__main__':
     heap_builder = HeapBuilder()
