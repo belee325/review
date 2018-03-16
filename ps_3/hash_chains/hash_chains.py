3a13206e5a2a840840f0a89d3deb050ea05ef4c3
Lee <blee13@metlife.com>
2018-03-16 14:22:52 -0400

    ps 3

--- ps_3/hash_chains/hash_chains.py
+++ ps_3/hash_chains/hash_chains.py
@@ -17,7 +17,7 @@ class QueryProcessor:
     def __init__(self, bucket_count):
         self.bucket_count = bucket_count
         # store all strings in one list
-        self.elems = []
+        self.elems = [[]] * bucket_count
 
     def _hash_func(self, s):
         ans = 0
@@ -25,9 +25,16 @@ class QueryProcessor:
             ans = (ans * self._multiplier + ord(c)) % self._prime
         return ans % self.bucket_count
 
-    def write_search_result(self, was_found):
-        print('yes' if was_found else 'no')
-
+    def write_search_result(self, ind, target):
+        if len(self.elems[ind]) == 0 :
+            print('no')
+            return
+        for word in self.elems[ind]:
+            if word == target:
+                print('yes')
+                return
+        print('no')
+        return
     def write_chain(self, chain):
         print(' '.join(chain))
 
@@ -37,22 +44,21 @@ class QueryProcessor:
     def process_query(self, query):
         if query.type == "check":
             # use reverse order, because we append strings to the end
-            self.write_chain(cur for cur in reversed(self.elems)
-                        if self._hash_func(cur) == query.ind)
+            self.write_chain(cur for cur in reversed(self.elems[query.ind]))
         else:
-            try:
-                ind = self.elems.index(query.s)
-            except ValueError:
-                ind = -1
+            ind = self._hash_func(query.s)
             if query.type == 'find':
-                self.write_search_result(ind != -1)
+                self.write_search_result(ind, query.s)
             elif query.type == 'add':
-                if ind == -1:
-                    self.elems.append(query.s)
+                try:
+                    self.elems[ind].index(query.s)
+                except ValueError:
+                    self.elems[ind].append(query.s)
             else:
-                if ind != -1:
-                    self.elems.pop(ind)
-
+                try:
+                    self.elems[ind].remove(query.s)
+                except ValueError:
+                    pass
     def process_queries(self):
         n = int(input())
         for i in range(n):
