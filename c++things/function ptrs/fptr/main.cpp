1a5072062094a7bb4449f8ccdae334d1c88c6588
belee325 <belee325@gmail.com>
2018-10-24 17:51:17 -0400

    func ptrs

---
+++ c++things/function ptrs/fptr/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+bool is_even(int num){
+	return (num % 2 == 0)? true:false;
+}
+void print(int num){
+	cout << "Hello: " << num << endl;
+}
+
+string make_string(int num){
+	stringstream ss;
+	ss << num;
+	string ret = ss.str();
+	return ret;
+}
+
+int main(int argc, char **argv)
+{
+	void (*f_ptr)(int) = print;
+	string (*f_ptr2)(int) = make_string;
+	f_ptr(7);
+	cout << f_ptr2(14) << endl;
+	vector<int> v;
+	for(int i =0; i < 11; i++){
+		v.push_back(i);
+	}
+	int num_even{count_if(v.begin(), v.end(), is_even)};
+	cout << "even elements in vector: " << num_even << endl; 
+	
+	int num_even_lambda{count_if(v.begin(), v.end(), [](int x){
+		return (x % 2 == 0)? true : false;})
+	};
+	cout << "even elements lambda in vector: " << num_even_lambda << endl;
+	return 0;
+}
